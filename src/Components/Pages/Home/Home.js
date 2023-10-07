c052486ad1a897a9cd468d785ad4e3a5496fe048
ArtLiathain <artp.oliathain@gmail.com>
2023-10-07 22:46:59 +0100

    U[date card component

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Card from "../../Card/Card";
+import styled from "./Home.module.css"
 
 const Home = () => {
   return (
-    <Card>
+    <Card classes={styled.home}>
       <h1>Home</h1>
     </Card>
   );
